cf70517c0c9411ce89281ff810f7c2441b63fe25
工藤颯斗 <kudo@tetemarche.co.jp>
2022-08-09 00:15:16 +0900

    wip

--- Makefile
+++ Makefile
@@ -1,109 +1,34 @@
-up:
-	docker compose up -d
-build:
-	docker compose build --no-cache --force-rm
-laravel-install:
-	docker compose exec app composer create-project --prefer-dist laravel/laravel .
-create-project:
-	mkdir -p src
-	@make build
-	@make up
-	@make laravel-install
-	docker compose exec app php artisan key:generate
-	docker compose exec app php artisan storage:link
-	docker compose exec app chmod -R 777 storage bootstrap/cache
-	@make fresh
-install-recommend-packages:
-	docker compose exec app composer require doctrine/dbal
-	docker compose exec app composer require --dev ucan-lab/laravel-dacapo
-	docker compose exec app composer require --dev barryvdh/laravel-ide-helper
-	docker compose exec app composer require --dev beyondcode/laravel-dump-server
-	docker compose exec app composer require --dev barryvdh/laravel-debugbar
-	docker compose exec app composer require --dev roave/security-advisories:dev-master
-	docker compose exec app php artisan vendor:publish --provider="BeyondCode\DumpServer\DumpServerServiceProvider"
-	docker compose exec app php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
-init:
-	docker compose up -d --build
-	docker compose exec app composer install
-	docker compose exec app cp .env.example .env
-	docker compose exec app php artisan key:generate
-	docker compose exec app php artisan storage:link
-	docker compose exec app chmod -R 777 storage bootstrap/cache
-	@make fresh
-remake:
-	@make destroy
-	@make init
-stop:
-	docker compose stop
-down:
-	docker compose down --remove-orphans
-down-v:
-	docker compose down --remove-orphans --volumes
-restart:
-	@make down
-	@make up
-destroy:
-	docker compose down --rmi all --volumes --remove-orphans
-ps:
-	docker compose ps
-logs:
-	docker compose logs
-logs-watch:
-	docker compose logs --follow
-log-web:
-	docker compose logs web
-log-web-watch:
-	docker compose logs --follow web
-log-app:
-	docker compose logs app
-log-app-watch:
-	docker compose logs --follow app
-log-db:
-	docker compose logs db
-log-db-watch:
-	docker compose logs --follow db
-web:
-	docker compose exec web bash
-app:
-	docker compose exec app bash
-migrate:
-	docker compose exec app php artisan migrate
-fresh:
-	docker compose exec app php artisan migrate:fresh --seed
-seed:
-	docker compose exec app php artisan db:seed
-dacapo:
-	docker compose exec app php artisan dacapo
-rollback-test:
-	docker compose exec app php artisan migrate:fresh
-	docker compose exec app php artisan migrate:refresh
-tinker:
-	docker compose exec app php artisan tinker
-test:
-	docker compose exec app php artisan test
-optimize:
-	docker compose exec app php artisan optimize
-optimize-clear:
-	docker compose exec app php artisan optimize:clear
-cache:
-	docker compose exec app composer dump-autoload -o
-	@make optimize
-	docker compose exec app php artisan event:cache
-	docker compose exec app php artisan view:cache
-cache-clear:
-	docker compose exec app composer clear-cache
-	@make optimize-clear
-	docker compose exec app php artisan event:clear
-db:
-	docker compose exec db bash
-sql:
-	docker compose exec db bash -c 'mysql -u $$MYSQL_USER -p$$MYSQL_PASSWORD $$MYSQL_DATABASE'
-redis:
-	docker compose exec redis redis-cli
-ide-helper:
-	docker compose exec app php artisan clear-compiled
-	docker compose exec app php artisan ide-helper:generate
-	docker compose exec app php artisan ide-helper:meta
-	docker compose exec app php artisan ide-helper:models --write
+.PHONY: default
+default: docker/up
+
+# for Development
+.PHONY: lint fmt test docker/*
+lint:
+	yarn prettier --check "**/*.php"
+
 fmt:
-	./src/vendor/bin/php-cs-fixer fix ./src -v --allow-risky=yes
+	yarn prettier --write "**/*.php"
+
+test:
+	yarn dredd
+	$(MAKE) docker/exec command="make test"
+
+docker/setup:
+	docker build --pull --tag read_worth_backend src/.
+	$(MAKE) docker/up
+	$(MAKE) docker/exec command="make setup"
+
+docker/up:
+	docker-compose up -d
+
+docker/stop:
+	docker-compose stop
+
+docker/down:
+	docker-compose down
+
+docker/exec: docker_option=
+docker/exec: service=app
+docker/exec: command=bash
+docker/exec:
+	docker-compose exec $(docker_option) $(service) $(command)
---
+++ database/schema.sql
@@ -0,0 +1,216 @@
+CREATE TABLE `book_category`
+(
+    `id`         bigint unsigned                         NOT NULL AUTO_INCREMENT,
+    `client_id`  bigint unsigned                         NOT NULL,
+    `name`       varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` timestamp                               NULL DEFAULT NULL,
+    `updated_at` timestamp                               NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `book_category_client_id_foreign` (`client_id`),
+    CONSTRAINT `book_category_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `book_histories`
+(
+    `id`         bigint unsigned                 NOT NULL AUTO_INCREMENT,
+    `user_id`    bigint unsigned                 NOT NULL,
+    `book_id`    bigint unsigned                 NOT NULL,
+    `action`     text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` timestamp                       NULL DEFAULT NULL,
+    `updated_at` timestamp                       NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `book_histories_user_id_foreign` (`user_id`),
+    KEY `book_histories_book_id_foreign` (`book_id`),
+    CONSTRAINT `book_histories_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+    CONSTRAINT `book_histories_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `book_purchase_applies`
+(
+    `id`         bigint unsigned                 NOT NULL AUTO_INCREMENT,
+    `user_id`    bigint unsigned                 NOT NULL,
+    `client_id`  bigint unsigned                 NOT NULL,
+    `book_id`    bigint unsigned                 NOT NULL,
+    `reason`     text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `price`      int                             NOT NULL,
+    `step`       smallint                        NOT NULL DEFAULT '1',
+    `location`   varchar(255) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `created_at` timestamp                       NULL     DEFAULT NULL,
+    `updated_at` timestamp                       NULL     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `book_applications_user_id_foreign` (`user_id`),
+    KEY `book_applications_client_id_foreign` (`client_id`),
+    KEY `book_applications_book_id_foreign` (`book_id`),
+    CONSTRAINT `book_applications_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+    CONSTRAINT `book_applications_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+    CONSTRAINT `book_applications_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+
+CREATE TABLE `book_rental_applies`
+(
+    `id`                   bigint unsigned                 NOT NULL AUTO_INCREMENT,
+    `user_id`              bigint unsigned                 NOT NULL,
+    `client_id`            bigint unsigned                 NOT NULL,
+    `book_id`              bigint unsigned                 NOT NULL,
+    `reason`               text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '申請理由',
+    `rental_date`          date                            NOT NULL COMMENT '貸出日',
+    `expected_return_date` date                            NOT NULL COMMENT '返却予定日',
+    `return_date`          date                                 DEFAULT NULL COMMENT '返却日',
+    `created_at`           timestamp                       NULL DEFAULT NULL,
+    `updated_at`           timestamp                       NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `book_rental_applies_user_id_foreign` (`user_id`),
+    KEY `book_rental_applies_client_id_foreign` (`client_id`),
+    KEY `book_rental_applies_book_id_foreign` (`book_id`),
+    CONSTRAINT `book_rental_applies_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+    CONSTRAINT `book_rental_applies_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+    CONSTRAINT `book_rental_applies_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+
+CREATE TABLE `book_reviews`
+(
+    `id`         bigint unsigned                 NOT NULL AUTO_INCREMENT,
+    `user_id`    bigint unsigned                 NOT NULL,
+    `book_id`    bigint unsigned                 NOT NULL,
+    `review`     text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `rate`       smallint                        NOT NULL,
+    `created_at` timestamp                       NULL DEFAULT NULL,
+    `updated_at` timestamp                       NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `book_reviews_user_id_foreign` (`user_id`),
+    KEY `book_reviews_book_id_foreign` (`book_id`),
+    CONSTRAINT `book_reviews_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+    CONSTRAINT `book_reviews_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `books`
+(
+    `id`               bigint unsigned                         NOT NULL AUTO_INCREMENT,
+    `client_id`        bigint unsigned                         NOT NULL,
+    `book_category_id` bigint unsigned                         NOT NULL,
+    `status`           smallint                                NOT NULL,
+    `title`            varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `description`      text COLLATE utf8mb4_unicode_ci,
+    `image_path`       varchar(255) COLLATE utf8mb4_unicode_ci      DEFAULT NULL,
+    `url`              text COLLATE utf8mb4_unicode_ci,
+    `created_at`       timestamp                               NULL DEFAULT NULL,
+    `updated_at`       timestamp                               NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `books_book_category_id_foreign` (`book_category_id`),
+    KEY `books_client_id_foreign` (`client_id`),
+    CONSTRAINT `books_book_category_id_foreign` FOREIGN KEY (`book_category_id`) REFERENCES `book_category` (`id`),
+    CONSTRAINT `books_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `clients`
+(
+    `id`                      bigint unsigned                         NOT NULL AUTO_INCREMENT,
+    `plan_id`                 bigint unsigned                         NOT NULL,
+    `name`                    varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `enable_purchase_limit`   tinyint(1)                              NOT NULL,
+    `purchase_limit`          int                                     NOT NULL,
+    `purchase_limit_unit`     varchar(20) COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT 'monthly',
+    `private_ownership_allow` tinyint(1)                              NOT NULL DEFAULT '0',
+    `created_at`              timestamp                               NULL     DEFAULT NULL,
+    `updated_at`              timestamp                               NULL     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `clients_name_unique` (`name`),
+    KEY `clients_plan_id_foreign` (`plan_id`),
+    CONSTRAINT `clients_plan_id_foreign` FOREIGN KEY (`plan_id`) REFERENCES `plans` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `password_resets`
+(
+    `email`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `token`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` timestamp                               NULL DEFAULT NULL,
+    KEY `password_resets_email_index` (`email`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `plans`
+(
+    `id`          bigint unsigned                        NOT NULL AUTO_INCREMENT,
+    `name`        varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'プラン名',
+    `price`       smallint                               NOT NULL COMMENT 'プラン価格',
+    `max_members` smallint                               NOT NULL COMMENT 'メンバー上限数',
+    `max_books`   smallint                               NOT NULL COMMENT '書籍上限数',
+    `created_at`  timestamp                              NULL DEFAULT NULL,
+    `updated_at`  timestamp                              NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `roles`
+(
+    `id`                 bigint unsigned NOT NULL AUTO_INCREMENT,
+    `user_id`            bigint unsigned NOT NULL,
+    `is_account_manager` tinyint(1)      NOT NULL DEFAULT '0',
+    `is_book_manager`    tinyint(1)      NOT NULL DEFAULT '0',
+    `is_client_manager`  tinyint(1)      NOT NULL DEFAULT '0',
+    `created_at`         timestamp       NULL     DEFAULT NULL,
+    `updated_at`         timestamp       NULL     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `roles_user_id_foreign` (`user_id`),
+    CONSTRAINT `roles_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `slack_credentials`
+(
+    `id`           bigint unsigned                         NOT NULL AUTO_INCREMENT,
+    `client_id`    bigint unsigned                         NOT NULL,
+    `access_token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `channel_id`   varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `channel_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at`   timestamp                               NULL DEFAULT NULL,
+    `updated_at`   timestamp                               NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `slack_credentials_client_id_foreign` (`client_id`),
+    CONSTRAINT `slack_credentials_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `users`
+(
+    `id`                  bigint unsigned                         NOT NULL AUTO_INCREMENT,
+    `client_id`           bigint unsigned                                               DEFAULT NULL,
+    `name`                varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email`               varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email_verified_at`   timestamp                               NULL                  DEFAULT NULL,
+    `password`            varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `password_setting_at` timestamp                               NULL                  DEFAULT NULL,
+    `google_access_token` varchar(255) COLLATE utf8mb4_unicode_ci                       DEFAULT NULL,
+    `purchase_balance`    int                                     NOT NULL,
+    `api_token`           varchar(80) COLLATE utf8mb4_unicode_ci                        DEFAULT NULL,
+    `remember_token`      varchar(100) COLLATE utf8mb4_unicode_ci                       DEFAULT NULL,
+    `created_at`          timestamp                               NULL                  DEFAULT NULL,
+    `updated_at`          timestamp                               NULL                  DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `users_email_unique` (`email`),
+    UNIQUE KEY `users_api_token_unique` (`api_token`),
+    KEY `users_client_id_foreign` (`client_id`),
+    CONSTRAINT `users_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
